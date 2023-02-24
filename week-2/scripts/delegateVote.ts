15c602e16c603f884549728f704bb39a5eba6cb0
Muu79 <muu794@gmail.com>
2023-02-24 19:30:30 +0200

    added a few comments

--- week-2/scripts/delegateVote.ts
+++ week-2/scripts/delegateVote.ts
@@ -14,7 +14,9 @@ function convertStringArrayToBytes32(array: string[]) {
   return bytes32Array;
 }
 
-async function delegateVote() {
+async function main() {
+  // We recive the delegate address and isDev which indicates if we 
+  // should use a localhost providor
   const [delegateAddress, isDev] = process.argv.slice(2, 4);
   const contractAddress: any = process.env.CONTRACT_ADDRESS;
   if (!contractAddress) throw new Error("Missing environment: Contract Address")
@@ -22,14 +24,15 @@ async function delegateVote() {
 
   let ballotContract, signer;
   //dev wallet setup
-  if (isDev == "true") {
+  if (isDev.toLowerCase() == "true") {
     signer = (await ethers.getSigners())[0]
     const ballotContractFactory = await ethers.getContractFactory("Ballot");
+    // We deploy a test contract then give the delegate the right to vote
     ballotContract = await ballotContractFactory.deploy(convertStringArrayToBytes32(["test1", "test2"]));
-    ballotContract.giveRightToVote(delegateAddress);
+    await ballotContract.giveRightToVote(delegateAddress);
   }
 
-  //test-net wallet setup
+  //test-net wallet and signer setup
   else {
     const provider = new ethers.providers.InfuraProvider("goerli", process.env.INFURA_API_KEY);
     const privateKey = process.env.PRIVATE_KEY;
@@ -39,16 +42,17 @@ async function delegateVote() {
     signer = wallet.connect(provider);
     ballotContract = new ethers.Contract(contractAddress, ballotABI, signer);
   }
-
-  const canDelegateVote = (await ballotContract.voters(delegateAddress))[3];
+  
   //Ballot.sol reverts without reason if delegate cannot vote
-  //so we catch this before reversion and return a meanigful error below
+  //so we catch this before reversion and return a meanigful error
+  const canDelegateVote = (await ballotContract.voters(delegateAddress))[3];
   if (!canDelegateVote) throw new Error("Contract error: Delegate address doesn't have the right to vote")
+  //
   const tx = await ballotContract.connect(signer).delegate(delegateAddress);
   console.log(tx);
 }
 
-delegateVote().catch(error => {
+main().catch(error => {
   console.log(error);
   process.exitCode = 1;
 });
