0ac66f8f93e51afa0dac6af258ca2217a8b1d243
stefan-b10 <72396423+stefan-b10@users.noreply.github.com>
2023-02-24 09:39:04 +0200

    Added paths for tests

--- week-2/hardhat.config.ts
+++ week-2/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  paths: { tests:"tests"},
   solidity: "0.8.17",
 };
 
