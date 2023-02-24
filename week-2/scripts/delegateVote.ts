e7c941357f8873993c67c9b0a5981f2019c9672f
Muu79 <muu794@gmail.com>
2023-02-24 15:51:42 +0200

    added basic delegateVote functionality.

---
+++ week-2/scripts/delegateVote.ts
@@ -0,0 +1,47 @@
+import { ethers } from "hardhat";
+
+const ballotABI = require("../contracts/Ballot.json")
+
+require("dotenv").config();
+
+function convertStringArrayToBytes32(array: string[]) {
+    const bytes32Array = [];
+    for (let index = 0; index < array.length; index++) {
+      bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+    }
+    return bytes32Array;
+  }
+
+async function delegateVote() {
+    const delegateAddress = process.argv[2];
+    const contractAddress:any = process.env.CONTRACT_ADDRESS;
+    if(!contractAddress) throw new Error("Missing environment: Contract Address")
+    if(!delegateAddress) throw new Error("Missing parameters: Address to delegate vote to")
+        
+    //dev wallet setup
+    /*
+    const signer = (await ethers.getSigners())[0]
+    const ballContractFactory = await ethers.getContractFactory("Ballot");
+    const ballContract = await ballContractFactory.deploy(convertStringArrayToBytes32(["test1", "test2"]));
+    */
+
+    //test-net wallet setup
+    const provider = new ethers.providers.InfuraProvider("goerli",process.env.INFURA_API_KEY);
+    const privateKey = process.env.PRIVATE_KEY;
+    if (!privateKey || privateKey.length <= 0) throw new Error("Missing environment: Private Key");
+    const wallet = new ethers.Wallet(privateKey);
+    console.log(`Connected to the wallet address ${wallet.address}`);
+    const signer = wallet.connect(provider);
+    const ballContract = new ethers.Contract(contractAddress, ballotABI, signer);
+    
+    
+    const canDelegateVote = (await ballContract.voters(delegateAddress))[1];
+    if(!canDelegateVote) throw new Error("Contract error: Delegate address doesn't have the right to vote")
+    const tx = await ballContract.connect(signer).delegate(delegateAddress);
+    console.log(tx);
+}
+
+delegateVote().catch(error=>{
+    console.log(error);
+    process.exitCode = 1;
+});
