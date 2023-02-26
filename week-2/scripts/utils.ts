93b422ea0df9be7bd4d8cbf9e7d1939da2a3adb0
Muu79 <muu794@gmail.com>
2023-02-26 18:48:12 +0200

    added fallbackProvidor function

--- week-2/scripts/utils.ts
+++ week-2/scripts/utils.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+require("dotenv").config()
 
 export function convertStringArrayToBytes32(array: string[]) {
   return array.map((str) => ethers.utils.formatBytes32String(str))
@@ -10,4 +11,12 @@ export function isAddressValid(address: string) {
 
 export function bytesToString(bytes: string) {
   return ethers.utils.parseBytes32String(bytes)
+}
+
+export function fallbackProvider() {
+  return new ethers.providers.FallbackProvider([
+    new ethers.providers.InfuraProvider('goerli', process.env.INFURA_API_KEY),
+    new ethers.providers.AlchemyProvider('goerli', process.env.ALCHEMY_API_KEY),
+    new ethers.providers.EtherscanProvider('goerli', process.env.ETHERSCAN_API_KEY),
+  ]);
 }
