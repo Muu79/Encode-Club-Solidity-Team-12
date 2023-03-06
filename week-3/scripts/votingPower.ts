4051a599585788169928d46a195b9076edff7dd6
adapole <eyassubn@gmail.com>
2023-03-06 11:16:58 +0300

    Fixed naming for votingPower.ts

--- week-3/scripts/votingPower.ts
+++ week-3/scripts/votingPower.ts
@@ -2,14 +2,13 @@ import { ethers } from 'hardhat';
 import * as dotenv from 'dotenv';
 import { MyToken__factory } from '../typechain-types';
 import { fallbackProvider, isPrivateKey } from './utils';
-import { BigNumber, BigNumberish } from 'ethers';
 dotenv.config();
 
 async function main() {
-	// getting 2 arguments: token contract address and delegatee address
+	// getting 2 arguments: token contract address and voters address
 	const args = process.argv.slice(2);
 	const tokenAddress = args[0];
-	let delegatee = args[1];
+	let voters = args[1];
 
 	// check if the arguments are valid addresses
 	if (!ethers.utils.isAddress(tokenAddress))
@@ -34,25 +33,21 @@ async function main() {
 	const wallet = new ethers.Wallet(privateKey, provider);
 	const signer = wallet.connect(provider);
 	console.log(`Connected to wallet address ${wallet.address}`);
-	if (!ethers.utils.isAddress(delegatee)) delegatee = wallet.address;
+	if (!ethers.utils.isAddress(voters)) voters = wallet.address;
 
 	// connecting to token contract
 	const MyTokenContractFactory = new MyToken__factory(signer);
 	const tokenContract = MyTokenContractFactory.attach(tokenAddress);
 	console.log(`Connected to token contract ${tokenContract.address}`);
 
-	// Check the voting power of delegatee
-	let tokenBalanceDelegatee = await tokenContract.balanceOf(delegatee);
+	// Check the voting power of voter's
+	let tokenBalanceVoter = await tokenContract.balanceOf(voters);
 	console.log(
-		`Delegatee has a balance of ${ethers.utils.formatEther(
-			tokenBalanceDelegatee
-		)}`
+		`Voter has a balance of ${ethers.utils.formatEther(tokenBalanceVoter)}`
 	);
-	let delegateeVotingPower = await tokenContract.getVotes(delegatee);
+	let votersVotingPower = await tokenContract.getVotes(voters);
 	console.log(
-		`Delegatee voting power is ${ethers.utils.formatEther(
-			delegateeVotingPower
-		)}`
+		`Voter's voting power is ${ethers.utils.formatEther(votersVotingPower)}`
 	);
 }
 
