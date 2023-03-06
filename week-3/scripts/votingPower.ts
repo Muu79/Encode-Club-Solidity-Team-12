a7fd0cb8352078b2d01bbbcb7d4d34974e665835
adapole <eyassubn@gmail.com>
2023-03-06 11:08:50 +0300

    New script, votingPower.ts

---
+++ week-3/scripts/votingPower.ts
@@ -0,0 +1,62 @@
+import { ethers } from 'hardhat';
+import * as dotenv from 'dotenv';
+import { MyToken__factory } from '../typechain-types';
+import { fallbackProvider, isPrivateKey } from './utils';
+import { BigNumber, BigNumberish } from 'ethers';
+dotenv.config();
+
+async function main() {
+	// getting 2 arguments: token contract address and delegatee address
+	const args = process.argv.slice(2);
+	const tokenAddress = args[0];
+	let delegatee = args[1];
+
+	// check if the arguments are valid addresses
+	if (!ethers.utils.isAddress(tokenAddress))
+		throw new Error(
+			`Parameter Error: Token contract address ${tokenAddress} is not a valid address`
+		);
+
+	// check if private key is valid
+	const privateKey = process.env.PRIVATE_KEY;
+	if (!privateKey) {
+		throw new Error(
+			`Environment Error: Please make sure your .env file contains a private key`
+		);
+	} else if (!isPrivateKey(privateKey)) {
+		throw new Error(
+			`Environment Error: Please make sure your private key in your .env file is a valid private key `
+		);
+	}
+
+	// connecting to provider and getting wallet
+	const provider = await fallbackProvider();
+	const wallet = new ethers.Wallet(privateKey, provider);
+	const signer = wallet.connect(provider);
+	console.log(`Connected to wallet address ${wallet.address}`);
+	if (!ethers.utils.isAddress(delegatee)) delegatee = wallet.address;
+
+	// connecting to token contract
+	const MyTokenContractFactory = new MyToken__factory(signer);
+	const tokenContract = MyTokenContractFactory.attach(tokenAddress);
+	console.log(`Connected to token contract ${tokenContract.address}`);
+
+	// Check the voting power of delegatee
+	let tokenBalanceDelegatee = await tokenContract.balanceOf(delegatee);
+	console.log(
+		`Delegatee has a balance of ${ethers.utils.formatEther(
+			tokenBalanceDelegatee
+		)}`
+	);
+	let delegateeVotingPower = await tokenContract.getVotes(delegatee);
+	console.log(
+		`Delegatee voting power is ${ethers.utils.formatEther(
+			delegateeVotingPower
+		)}`
+	);
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
