81af9b5e2b2d78b3a5937ac88394c2e05836d283
stefan-b10 <72396423+stefan-b10@users.noreply.github.com>
2023-03-06 11:01:53 +0200

    giveVotingTokens
    added giveVotingTokens script

---
+++ week-3/scripts/giveVotingTokens.ts
@@ -0,0 +1,44 @@
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+import { MyToken__factory } from "../typechain-types";
+
+dotenv.config();
+
+async function main() {
+  // require 3 arguments, 1.token contract address 2.address to receive tokens 3.amount of tokens the address receives
+  const args = process.argv.slice(2);
+  const tokenAddress = args[0];
+  const voter = args[1];
+  const amount = ethers.utils.parseEther(args[2]);
+
+  // check if arguments are valid
+  if (!ethers.utils.isAddress(tokenAddress)) throw new Error(`${tokenAddress} is not a valid address!`);
+
+  if (!ethers.utils.isAddress(voter)) throw new Error(`${voter} is not a valid address!`);
+
+  if (!ethers.BigNumber.isBigNumber(amount)) throw new Error(`${amount} is not a valid number!`);
+
+  // connecting to wallet and provider
+  const provider = ethers.getDefaultProvider("goerli");
+
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey || privateKey.length <= 0) throw new Error("Missing environment: Private key");
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  // initialise the contract
+  const tokenContractFactory = new MyToken__factory(signer);
+  const tokenContract = tokenContractFactory.attach(tokenAddress);
+
+  console.log(`Connected to contract ${tokenAddress}`);
+
+  const mintTx = await tokenContract.mint(voter, amount);
+  await mintTx.wait();
+  const tokenBalance = await tokenContract.balanceOf(voter);
+  console.log(`${voter} received ${ethers.utils.formatEther(tokenBalance)} Voting Tokens`);
+}
+
+main().catch((error) => {
+  console.log(error);
+  process.exitCode = 1;
+});
