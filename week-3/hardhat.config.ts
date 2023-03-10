bb452790e30588e7e15037238926c2a82ab327ea
3Spear <kalebbirru@gmail.com>
2023-03-10 19:26:09 +0300

    Verify Token Contract on Etherscan via
    The contract 0x4bD47dAa4f1fee21C97549e4C396A16e58fA6635 has already been verified

--- week-3/hardhat.config.ts
+++ week-3/hardhat.config.ts
@@ -1,9 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   paths: { tests:"tests"},
   solidity: "0.8.17",
+  networks: {
+	  goerli: {
+		  url: process.env.ALCHEMY_GOERLI_URL,
+		  accounts: [process.env.PRIVATE_KEY],
+	  },
+  },
+  etherscan: {
+	  apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
