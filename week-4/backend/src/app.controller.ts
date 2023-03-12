f22451e9a65736c6d3594390e8d6ec08ffb881d0
stefan-b10 <stefan_budai88@yahoo.com>
2023-03-12 20:12:00 +0200

    modified:   week-4/backend/src/app.controller.ts modified:   week-4/backend/src/app.service.ts new file:   week-4/backend/src/dtos/castVote.dto.ts

--- week-4/backend/src/app.controller.ts
+++ week-4/backend/src/app.controller.ts
@@ -1,27 +1,32 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { AppService } from './app.service';
-import { MintDTO } from './dtos/mint.dto';
-
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { CastVoteDTO } from "./dtos/castVote.dto";
+import { MintDTO } from "./dtos/mint.dto";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
+
   // Ballot functions
 
-  @Get('voting-power')
-  async votingPower(
-    @Query('ballotAddress') ballotAddress: string,
-    @Query('address') address: string,
-  ): Promise<object> {
+  @Get("voting-power")
+  async votingPower(@Query("ballotAddress") ballotAddress: string, @Query("address") address: string): Promise<object> {
     return await this.appService.votingPower(ballotAddress, address);
+  }
+  @Get("winning-proposal")
+  async winningProposal(@Query("ballotAddress") ballotAddress: string): Promise<object> {
+    return await this.appService.winningProposal(ballotAddress);
+  }
+
+  @Post("cast-vote")
+  async castVote(@Body() body: CastVoteDTO): Promise<string> {
+    return await this.appService.castVote(body.ballotAddress, body.proposal, body.amount);
   }
 
   // Token functions
 
-  @Post('mint')
-  mint(@Body() body: 
-  MintDTO) {
+  @Post("mint")
+  mint(@Body() body: MintDTO) {
     return this.appService.mint(body.to, body.amount.toString());
   }
 }
--- week-4/backend/src/app.service.ts
+++ week-4/backend/src/app.service.ts
@@ -1,8 +1,8 @@
-import * as dotenv from 'dotenv';
-import { Injectable } from '@nestjs/common';
-import { BigNumber, ethers } from 'ethers';
-import * as tokenJson from './assets/MyToken.json';
-import * as ballotJson from './assets/Ballot.json';
+import * as dotenv from "dotenv";
+import { Injectable } from "@nestjs/common";
+import { BigNumber, ethers } from "ethers";
+import * as tokenJson from "./assets/MyToken.json";
+import * as ballotJson from "./assets/Ballot.json";
 
 dotenv.config();
 
@@ -12,44 +12,73 @@ export class AppService {
   tokenContract: ethers.Contract;
 
   constructor() {
-    this.provider = ethers.getDefaultProvider('goerli');  
-    this.tokenContract = new ethers.Contract(
-      process.env.TOKEN_CONTRACT_ADDRESS,
-      tokenJson.abi,
-      this.provider,
-    );  
+    this.provider = ethers.getDefaultProvider("goerli");
+    this.tokenContract = new ethers.Contract(process.env.TOKEN_CONTRACT_ADDRESS, tokenJson.abi, this.provider);
   }
-  
+
   // Ballot
 
   async votingPower(ballotAddress: string, address: string): Promise<object> {
     // check if parameters are valid addresses
-    if (!ethers.utils.isAddress(ballotAddress))
-      throw new Error(
-        `Parameter Error: Token contract address ${ballotAddress} is not a valid address`,
-      );
-    if (!ethers.utils.isAddress(address))
-      throw new Error(
-        `Parameter Error: Token contract address ${address} is not a valid address`,
-      );
+    if (!ethers.utils.isAddress(ballotAddress)) throw new Error(`Parameter Error: Token contract address ${ballotAddress} is not a valid address`);
+    if (!ethers.utils.isAddress(address)) throw new Error(`Parameter Error: Token contract address ${address} is not a valid address`);
 
     // connecting to Ballot contract
-    const ballotContract = new ethers.Contract(
-      ballotAddress,
-      ballotJson.abi,
-      this.provider,
-    );
+    const ballotContract = new ethers.Contract(ballotAddress, ballotJson.abi, this.provider);
 
     // check the voting power
     const votingPowerBN = await ballotContract.votingPower(address);
     const votingPowerSpentBN = await ballotContract.votingPowerSpent(address);
     const votingPower = ethers.utils.formatEther(votingPowerBN);
     const votingPowerSpent = ethers.utils.formatEther(votingPowerSpentBN);
-    console.log(votingPower, votingPowerSpent);
 
     return { votingPower, votingPowerSpent };
   }
-   
+
+  async winningProposal(ballotAddress: string): Promise<object> {
+    // check if parameter is a valid address
+    if (!ethers.utils.isAddress(ballotAddress)) throw new Error(`Parameter Error: Token contract address ${ballotAddress} is not a valid address`);
+
+    // connecting to Ballot contract
+    const ballotContract = new ethers.Contract(ballotAddress, ballotJson.abi, this.provider);
+
+    // querying result
+    const winnerProposalIndexBN = await ballotContract.winningProposal();
+    const winnerProposalName = ethers.utils.parseBytes32String((await ballotContract.proposals(winnerProposalIndexBN)).name);
+    const winnerVotecount = ethers.utils.formatEther((await ballotContract.proposals(winnerProposalIndexBN))?.voteCount);
+
+    if (winnerVotecount === ethers.utils.formatEther(0)) {
+      throw new Error("No vote has been casted yet");
+    }
+
+    const winnerProposalIndex = parseFloat(winnerProposalIndexBN);
+
+    console.log(winnerProposalName);
+    return {
+      winnerProposalIndex: winnerProposalIndex,
+      winnerProposalName: winnerProposalName,
+      winnerVotecount: winnerVotecount,
+    };
+  }
+
+  async castVote(ballotAddress: string, proposal: number, amount: string): Promise<string> {
+    // check if parameter is a valid address
+    if (!ethers.utils.isAddress(ballotAddress)) throw new Error(`Parameter Error: Token contract address ${ballotAddress} is not a valid address`);
+
+    // connecting to Ballot contract
+    const ballotContract = new ethers.Contract(ballotAddress, ballotJson.abi, this.provider);
+
+    // connecting to wallet nad getting signer
+    const privateKey = process.env.PRIVATE_KEY;
+    const wallet = new ethers.Wallet(privateKey);
+    const signer = wallet.connect(this.provider);
+
+    // voting
+    const voteTx = await ballotContract.connect(signer).vote(proposal, ethers.utils.parseEther(amount));
+    const txReceipt = await voteTx.wait();
+    return txReceipt;
+  }
+
   // Token
 
   async mint(to: string, amount: string) {
@@ -58,7 +87,6 @@ export class AppService {
     const signer = wallet.connect(this.provider);
     const tx = await this.tokenContract.connect(signer).mint(to, ethers.utils.parseEther(amount));
     const txReceipt = await tx.wait();
-    return txReceipt.status == 1 ? 'Completed' : 'Reverted';
+    return txReceipt.status == 1 ? "Completed" : "Reverted";
   }
-
 }
