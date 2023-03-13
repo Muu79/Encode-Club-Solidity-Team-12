f210c02499c16cbc329b633d091d344b7e1063af
Muu79 <35148226+Muu79@users.noreply.github.com>
2023-03-13 21:06:32 +0200

    Manual edit for merge

--- week-4/frontend/app/results/page.tsx
+++ week-4/frontend/app/results/page.tsx
@@ -4,15 +4,30 @@ import { useEffect, useState } from "react";
 
 const page = () => {
 	const [resultArray, setResArr] = useState<Result[]>([]);
+	const [winner, setWinner] = useState<Result>({
+		name: "",
+		index: 0,
+		voteTotal: ""
+	});
 
 	useEffect(() => {
 		const getResults = async () => {
 			const response = await fetch('./api/get-proposals/route.ts');
 			const data = await response.json();
 			const proposals = data.response.proposals;
-			console.log(proposals);
+			let localWinner : Result = {
+				name: "",
+				index: 0,
+				voteTotal: ""
+			};
 
 			setResArr(proposals.map((proposal: { name: string, votes: string }, index: number) => {
+				if (localWinner.name === "" || Number(localWinner.voteTotal) < Number(proposal.votes)) localWinner = {
+					voteTotal: proposal.votes,
+					index: index,
+					name: proposal.name
+				};
+
 				return {
 					name: proposal.name,
 					voteTotal: proposal.votes,
@@ -20,14 +35,22 @@ const page = () => {
 				}
 			}))
 
+			setWinner(localWinner);
 
 		}
 		getResults();
 	}, [])
 	return (
-		<div className="flex flex-col w-full mt-2">
-			<h1 className="text-3xl text-center">Results Page</h1>
+		<div className="flex flex-col w-full mt-10">
+			<h1 className="text-5xl text-center">Results Page</h1>
 			<Results results={resultArray} />
+			<h1 className="text-center flex flex-col m-5">
+				{winner.voteTotal !== "" &&
+					(<>
+						<span className="m-3 text-3xl">The winner is {winner?.name}</span>
+						<span className="m-3 text-3xl">With a vote total of {winner?.voteTotal}</span>
+					</>)}
+			</h1>
 		</div>
 	);
 };
