d1ed450ea943b3648798b0fa57260e93d9f1c960
Muu79 <muu794@gmail.com>
2023-03-13 19:22:19 +0200

    hardcoded ballot contract into API call

--- week-4/frontend/app/api/get-proposals/[ballot]/route.ts
+++ week-4/frontend/app/api/get-proposals/[ballot]/route.ts
@@ -2,9 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: any) {
 	try {
-		const ballotAddress = params.ballot;
 
-		//body: JSON.stringify({ballotAddress: "0x9B93774789584f3c665202Ee0609dDEfF5Cfee30"})
+		const ballotAddress = "0xc3B172c55e328E979cf2668C2eeE1c1b399e37D6"
 		const res = await fetch(
 			`http://localhost:3000/get-proposals?ballotAddress=${ballotAddress}`,
 			{
