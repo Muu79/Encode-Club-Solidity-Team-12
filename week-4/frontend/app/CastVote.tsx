696bca6686943ac4871576b9333d91851ce98103
adapole <eyassubn@gmail.com>
2023-03-13 20:33:44 +0300

    Cast vote integration with API, 80% done

--- week-4/frontend/app/CastVote.tsx
+++ week-4/frontend/app/CastVote.tsx
@@ -4,6 +4,7 @@ import { InputField, PrimaryBtn } from '@/components/HtmlElements';
 import { ethers } from 'ethers';
 import { useConnectWallet } from '@web3-onboard/react';
 import { SetStateAction, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const CastVote = () => {
 	const [{ wallet }] = useConnectWallet();
@@ -16,24 +17,42 @@ const CastVote = () => {
 		proposal: number,
 		ballotAddress: string
 	) {
-		if (wallet && voteAmount > 0 && voteIndex > 0) {
+		if (wallet && voteAmount > 0) {
 			// API call to backend
 			// request(castVote, amount, proposal, ballotAddress)
-
-			ethersProvider = new ethers.providers.Web3Provider(
-				wallet.provider,
-				'any'
+			const notification = toast.loading(
+				`Casting ${amount} votes for ${ballotAddress.substring(0, 6)}`
 			);
+			try {
+				const res = await fetch(
+					`api/cast-vote/${ballotAddress}/${proposal}/${amount}`,
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					}
+				);
+				const data = await res.json();
+				console.log(data);
+				ethersProvider = new ethers.providers.Web3Provider(
+					wallet.provider,
+					'any'
+				);
 
-			const signer = ethersProvider.getSigner();
+				const signer = ethersProvider.getSigner();
 
-			const txn = await signer.sendTransaction({
-				to: '0x109bf5e11140772a1427162bb51e23c244d13b88',
-				data: '<unsigned hash from backend>',
-			});
+				const txn = await signer.sendTransaction({
+					to: ballotAddress,
+					data: data,
+				});
 
-			const receipt = await txn.wait();
-			console.log(receipt);
+				const receipt = await txn.wait();
+				console.log(receipt);
+				toast.success('Successfully Voted!', { id: notification });
+			} catch (error) {
+				toast.error('Whoops... You didnt vote!', { id: notification });
+			}
 		}
 	}
 
@@ -62,7 +81,14 @@ const CastVote = () => {
 			/>
 			<PrimaryBtn
 				name='Cast Vote'
-				onClick={() => castVote(voteAmount, voteIndex, 'BallotAddress')}
+				onClick={() =>
+					castVote(
+						voteAmount,
+						voteIndex,
+						process.env.NEXT_PUBLIC_CONTRACT ||
+							'0x9B93774789584f3c665202Ee0609dDEfF5Cfee30'
+					)
+				}
 			/>
 		</>
 	);
