8ffe9d6a697c27c960ce620d4d635eed0548825e
adapole <eyassubn@gmail.com>
2023-03-20 18:56:56 +0300

    ThreeJs experimental UI, and Overlay part on top of the canvas (#35)
    lotteryui branch merged with dev branch

---
+++ week-5/next-app/app/ThreePage.tsx
@@ -0,0 +1,100 @@
+'use client';
+import {
+	CubeCamera,
+	Environment,
+	OrbitControls,
+	PerspectiveCamera,
+} from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import {
+	EffectComposer,
+	DepthOfField,
+	Bloom,
+	ChromaticAberration,
+} from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
+import { Suspense } from 'react';
+import { Boxes } from './Boxes';
+import { Car } from './Car';
+import { Ground } from './Ground';
+
+function CarModel() {
+	return (
+		<>
+			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+			{/* <PerspectiveCamera
+				makeDefault
+				fov={50}
+				position={[3, 2, 5]}
+				/> */}
+			{/* <mesh>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshBasicMaterial color={'red'} />
+			</mesh> */}
+			<color args={[0, 0, 0]} attach='background' />
+			{/* <CubeCamera resoultion={256} frames={Infinity}>
+				{(texture) => (
+					<>
+						<Environment map={texture} />
+						
+					</>
+				)}
+			</CubeCamera> */}
+
+			<spotLight
+				color={[1, 0.25, 0.7]}
+				intensity={1.5}
+				angle={0.6}
+				penumbra={0.5}
+				position={[5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<spotLight
+				color={[0.14, 0.5, 1]}
+				intensity={2}
+				angle={0.6}
+				penumbra={0.5}
+				position={[-5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<Ground />
+			<Car />
+			<Boxes />
+			<EffectComposer>
+				<DepthOfField
+					focusDistance={0.0035}
+					focalLength={0.01}
+					bokehScale={3}
+					height={480}
+				/>
+				<Bloom
+					blendFunction={BlendFunction.ADD}
+					intensity={1.3} // The bloom intensity.
+					width={300} // render width
+					height={300} // render height
+					kernelSize={5} // blur kernel size
+					luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+					luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+				/>
+				<ChromaticAberration
+					blendFunction={BlendFunction.NORMAL} // blend mode
+					// offset={[0.0005, 0.0012]} // color offset
+				/>
+			</EffectComposer>
+		</>
+	);
+}
+export default function ThreePage({ children }: { children: React.ReactNode }) {
+	return (
+		<div>
+			<Suspense fallback={<p>Loading models...</p>}>
+				{children}
+				<Canvas shadows>
+					<CarModel />
+				</Canvas>
+			</Suspense>
+		</div>
+	);
+}
