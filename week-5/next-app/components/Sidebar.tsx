b94dcda9d8947bbc408401e001e4b0fc7a47e341
Muu79 <muu794@gmail.com>
2023-03-20 02:49:09 +0200

    larger logo

--- week-5/next-app/components/Sidebar.tsx
+++ week-5/next-app/components/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
 	return (
 		<div className='h-screen p-2 md:p-10 py-6 overflow-y-auto border-indigo-500/50'>
 			<div className='flex flex-col items-center justify-center mb-10'>
-				<GiRollingDices size={40} />
+				<GiRollingDices size={60} className={'-mb-10'} />
 				<h1 className='hidden md:inline text-center text-3xl mt-2'>Lottery</h1>
 				<h2 className='hidden md:inline text-center text-xs italic'>Dapp</h2>
 			</div>
