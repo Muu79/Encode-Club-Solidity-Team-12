fd98e418a7906dafea8b1c98079bc2a92cf145f2
Muu79 <muu794@gmail.com>
2023-03-20 16:15:24 +0200

    unintentional issue with UI

--- week-5/next-app/components/HtmlElements.tsx
+++ week-5/next-app/components/HtmlElements.tsx
@@ -20,7 +20,7 @@ export const PrimaryBtn = ({ onClick, name, className }: PrimaryBtnProps) => (
 );
 
 export const InputField = ({ inputType, placeholder, onChange, className }: InputFieldProps) => (
-  <div className="w-1/2">
+  <div>
     <input
       type={inputType}
       className={`${className ? className + " " : ""}`+"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full mt-3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
